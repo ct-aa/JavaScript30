df09577a5b94861072dcd0356909d243f50155c4
Alex@MBP2019 <alex.alfred.villanueva@codeandtheory.com>
2021-11-09 17:58:37 +0800

    ✨ Add Draggable Progress Bar Function

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -2,33 +2,49 @@ const videoSource = document.querySelector(".viewer");
 const videoTriggers = document.querySelectorAll(".viewer, .toggle");
 const videoSliders = document.querySelectorAll(".player__slider");
 const videoSkipper = document.querySelectorAll("button[data-skip]");
-const videoProgress = document.querySelector(".progress__filled");
+const videoProgressBar = document.querySelector(".progress__filled");
+const videoProgress = document.querySelector(".progress");
+const playIcon = document.querySelector(".toggle");
 
 let isPlaying = false;
+let progressTime = 0;
 
 const handlePlay = () => {
-  isPlaying == false ? `${videoSource.play()}` : `${videoSource.pause()}`;
+  isPlaying === false
+    ? `${videoSource.play()} ${(playIcon.textContent = "❚ ❚")}`
+    : `${videoSource.pause()} ${(playIcon.textContent = "►")}`;
   isPlaying = !isPlaying;
 };
 
-const handleProgress = () => {
-  let progressTime =
+const handleProgressTime = () => {
+  progressTime =
     ((videoSource.currentTime / videoSource.duration) * 100).toString() + "%";
-  videoProgress.style.flexBasis = progressTime;
+  videoProgressBar.style.flexBasis = progressTime;
+};
+
+const handleProgressBar = (e) => {
+  const movieProgress =
+    (e.offsetX / videoProgress.offsetWidth) * videoSource.duration;
+  videoSource.currentTime = movieProgress;
 };
 
 const handleSlider = (e) => (videoSource[e.target.name] = e.target.value);
+
 const handleSkip = (e) =>
   (videoSource.currentTime += parseInt(e.target.getAttribute("data-skip")));
 
 videoTriggers.forEach((trigger) =>
   trigger.addEventListener("click", handlePlay)
 );
+
+videoSource.addEventListener("timeupdate", handleProgressTime);
+
+videoProgress.addEventListener("click", handleProgressBar);
+
 videoSliders.forEach((slider) =>
   slider.addEventListener("mousemove", handleSlider)
 );
+
 videoSkipper.forEach((skipper) =>
   skipper.addEventListener("click", handleSkip)
 );
-
-videoSource.addEventListener("timeupdate", handleProgress);
