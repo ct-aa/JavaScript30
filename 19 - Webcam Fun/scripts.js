49997281cf7228c72726bda14d8fe0ef50034e44
Alex@MBP2019 <alex.alfred.villanueva@codeandtheory.com>
2021-11-18 12:56:37 +0800

    ✨ Add function for rgb filter

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,7 +3,8 @@ const canvas = document.querySelector(".photo");
 const ctx = canvas.getContext("2d");
 const strip = document.querySelector(".strip");
 const snap = document.querySelector(".snap");
-const photoContent = [];
+
+const screenshots = [];
 
 const constraints = {
   video: true,
@@ -27,19 +28,36 @@ const drawVideo = () => {
 
   return setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height);
+    const currentVideo = ctx.getImageData(0, 0, width, height);
+    const updatedVideo = rgbSpliter(currentVideo, {
+      rOffset: -250,
+      gOffset: 500,
+      bOffset: -350,
+    });
+    ctx.putImageData(updatedVideo, 0, 0);
   }, 0);
+};
+
+const rgbSpliter = (currentVideo, rgbOffset) => {
+  const { rOffset, gOffset, bOffset } = rgbOffset;
+  for (let i = 0; i < currentVideo.data.length; i += 4) {
+    currentVideo.data[i + rOffset] = currentVideo.data[i + 0];
+    currentVideo.data[i + gOffset] = currentVideo.data[i + 1];
+    currentVideo.data[i + bOffset] = currentVideo.data[i + 2];
+  }
+  return currentVideo;
 };
 
 const takePhoto = () => {
   snap.currentTime = 0;
   snap.play();
 
-  const data = canvas.toDataURL("image/jpeg");
-  photoContent.unshift(
-    `<a href="${data}" download="handsome"><img src="${data}" alt="Handsome Man" /></a>`
+  const photo = canvas.toDataURL("image/jpeg");
+  screenshots.unshift(
+    `<a href="${photo}" download="handsome"><img src="${photo}"/></a>`
   );
 
-  strip.innerHTML = photoContent;
+  strip.innerHTML = screenshots;
 };
 
 window.addEventListener("load", getVideo);
